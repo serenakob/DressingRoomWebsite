a6d6ea1f179e683c0888b171eabda475dbd61c5a
serenakob <114862871+serenakob@users.noreply.github.com>
2023-06-05 10:55:58 +0300

    Add files via upload

---
+++ site.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="utf-8">
+        <title></title>
+    </head> 
+    
+    <body>
+    
+        <form action="site.php" method= "get">
+            Name: <input type="text" name= "name">
+            <br>
+            Age : <input type="number" name = "age">
+            <input type="submit">
+        </form>
+        <br>
+
+        Your name is <?php echo $_GET["name"]?>
+        <br>
+        Your age is <?php echo $_GET["age"]?>
+    </body> 
+</html>
